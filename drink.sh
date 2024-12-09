377cdd14fc4822318e77eedf7d745404b4fa6103
XoanOuteiro <115654591+XoanOuteiro@users.noreply.github.com>
2024-12-09 18:13:20 +0100

    Added yay to base image + some minor branding

--- drink.sh
+++ drink.sh
@@ -24,8 +24,8 @@ IMAGE_EXISTS=$(docker images -q "$IMAGE_NAME")
 
 # If the image does not exist, show available flavors
 if [ -z "$IMAGE_EXISTS" ]; then
-  echo "Brew for flavor '$FLAVOR' not found. But you can brew these teas:"
-  ls ./flavors/
+  echo "Brew for flavor '$FLAVOR' not found. But you have these ones:"
+  echo $(docker images | grep tea | awk -F ' ' '{print $2}' | grep -v base)
   exit 1
 fi
 
@@ -34,12 +34,12 @@ CONTAINER_EXISTS=$(docker ps -a -q -f name="$CONTAINER_NAME")
 
 if [ -z "$CONTAINER_EXISTS" ]; then
   # If the container does not exist, create and start it
-  echo "Tea for flavor '$FLAVOR' not found. Creating and starting container..."
+  echo "Tea for brew '$FLAVOR' not found. Creating and starting tea..."
   docker run -it --name "$CONTAINER_NAME" --hostname "$FLAVOR" "$IMAGE_NAME" zsh
   echo "Tea '$CONTAINER_NAME' started."
 else
   # If the container exists, start it and attach to it
-  echo "Tea for flavor '$FLAVOR' already exists. Starting and attaching..."
+  echo "Tea for brew '$FLAVOR' already exists. Starting and attaching..."
   docker start "$CONTAINER_NAME"
   docker attach "$CONTAINER_NAME"
 fi
